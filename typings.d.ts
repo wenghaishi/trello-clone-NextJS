73d356387166694d432ed6edbf8fc4f3e9948f45
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-06-05 02:47:01 +0800

    set up typings

---
+++ typings.d.ts
@@ -0,0 +1,23 @@
+interface Board {
+  columns: Map<TypedColumn, Column>;
+}
+
+type TypedColumn = "todo" | "inprogress" | "done";
+
+interface Column {
+  id: TypedColumns;
+  todos: Todo[];
+}
+
+interface Todo extends Models.Document {
+  $id: string;
+  $createdAt: string;
+  title: string;
+  status: string;
+  image?: string;
+}
+
+interface Image {
+  bucketId: string;
+  fileId: string;
+}
