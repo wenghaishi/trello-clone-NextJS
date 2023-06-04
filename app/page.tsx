799307cc9769ab43b47b25254a5629d695c8e0bb
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-06-05 01:32:48 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+import Board from "@/components/Board";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Board />
+    </main>
+  );
+}
