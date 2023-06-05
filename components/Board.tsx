27a5240dcfaff9742f1cb857c48ef4c3a3956c3c
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-06-05 11:33:52 +0800

    created gettodosbygroupedcolumn component

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,27 +1,22 @@
-'use client'
+"use client";
+import { useBoardStore } from "@/store/BoardStore";
 import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function Board() {
+  const getBoard = useBoardStore((state) => state.getBoard);
 
   useEffect(() => {
-      
-  })
+    getBoard();
+  }, [getBoard]);
 
   return (
-    <DragDropContext>
-      <Droppable
-        droppableId="board"
-        direction="horizontal"
-        type="column"
-      >
-        {(provided) => (
-          <div>
-            
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <h1>hi</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div></div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 }
 
