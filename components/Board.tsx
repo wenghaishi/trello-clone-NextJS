3462db54807746b9f584484b1be75d32783e551b
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-06-07 13:26:54 +0800

    implemented logic for dragging tasks to other columns

--- components/Board.tsx
+++ components/Board.tsx
@@ -66,9 +66,25 @@ function Board() {
       const newColumns = new Map(board.columns);
       newColumns.set(startCol.id, newCol);
 
-      setBoardState({...board, columns: newColumns})
+      setBoardState({ ...board, columns: newColumns });
     } else {
       // different column task drag
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+
+      setBoardState({ ...board, columns: newColumns });
     }
   };
 
