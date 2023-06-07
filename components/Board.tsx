fef89ff3e9024c10b3122d7466249c2947426623
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-06-07 13:39:23 +0800

    created method to update DB

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,11 +5,14 @@ import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
 import Column from "./Column";
 
 function Board() {
-  const [board, getBoard, setBoardState] = useBoardStore((state) => [
-    state.board,
-    state.getBoard,
-    state.setBoardState,
-  ]);
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+    (state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTodoInDB,
+    ]
+  );
 
   useEffect(() => {
     getBoard();
@@ -83,6 +86,9 @@ function Board() {
         id: finishCol.id,
         todos: finishTodos,
       });
+
+      // update in db
+      updateTodoInDB(todoMoved, finishCol.id);
 
       setBoardState({ ...board, columns: newColumns });
     }
